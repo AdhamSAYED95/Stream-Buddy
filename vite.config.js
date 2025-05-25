dec1b68a44db2144cd7608add906e6f734205750
AdhamSAYED95 <96503893+AdhamSAYED95@users.noreply.github.com>
2025-05-25 13:33:50 +0300

    create Brackets View

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import electron from 'vite-plugin-electron'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   base: './',
   plugins: [
     vue(),
-    vueDevTools(),
     vuetify({
       autoImport: true,
     }),
